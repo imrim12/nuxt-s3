e1cb0999966b7817d079e223676b678a65fd47cb
kst-y-nguyen <y.nguyen@konsento.ch>
2023-08-07 17:59:38 +0700

    fix: init project :tada:

---
+++ src/module.ts
@@ -0,0 +1,25 @@
+import { defineNuxtModule, addServerHandler, createResolver } from '@nuxt/kit'
+
+// Module options TypeScript interface definition
+export interface ModuleOptions {}
+
+export default defineNuxtModule<ModuleOptions>({
+  meta: {
+    name: 'nuxt-s3-client',
+    configKey: 's3'
+  },
+  // Default configuration options of the Nuxt module
+  defaults: {},
+  setup () {
+    const resolver = createResolver(import.meta.url)
+
+    // Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
+    addServerHandler({
+      route: '/api/s3',
+      method: 'PUT',
+      middleware: true,
+      lazy: true,
+      handler: resolver.resolve('./runtime/handler')
+    })
+  }
+})
